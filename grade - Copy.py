ba5bed5ddf1feab573772f842f9dd2fc62d108b9
preash-cyber <azgakupatience2017@gmail.com>
2020-04-03 13:37:54 +0100

    first commit

---
+++ grade - Copy.py
@@ -0,0 +1,11 @@
+average = int(input('what is your score?\n>>'))
+if average >= 70 and average <= 100:
+    print('your grade is A')
+elif average >= 60 and average <=69:
+    print('your grade is B')
+elif average >= 50 and average <=59:
+    print('your grade is C')
+elif average >=50 and average <=59:
+    print('your grade is D')
+else:
+    print('your grade is f')
